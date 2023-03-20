302679b608bf322f5b7dd0f27cdda91367423392
Jiang <114283194+jiang0524@users.noreply.github.com>
2023-03-20 23:29:00 +0700

    Add files via upload

---
+++ BaiTapWindowsForms/BaiTapWindowsForms/frmBai2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapWindowsForms
+{
+    public partial class frmBai2 : Form
+    {
+        public frmBai2()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBai2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbbTenHang_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var stt = cbbTenHang.SelectedIndex;
+            switch(stt)
+            {
+                //0,1,2 là vị trí các mặt hàng trong danh sách
+                case 0:
+                    txtDonGia.Text = "100000";
+                    break;
+                case 1:
+                    txtDonGia.Text = "2000000";
+                    break;
+                case 2:
+                    txtDonGia.Text = "150000";
+                    break;
+            }    
+        }
+
+        private void btnTinhTien_Click(object sender, EventArgs e)
+        {
+            int donGia = int.Parse(txtDonGia.Text);
+            int soLuong = int.Parse(txtSoLuong.Text);
+            double thanhTien = donGia * soLuong;
+
+
+            if (rdChuyenKhoan.Checked)
+                thanhTien = donGia * soLuong * 0.95;
+
+            lblSoTien.Text = thanhTien.ToString();
+        }
+    }
+}
---
+++ BaiTapWindowsForms/BaiTapWindowsForms/frmBai3.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace BaiTapWindowsForms
+{
+    partial class frmBai3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSoA = new System.Windows.Forms.TextBox();
+            this.txtSoB = new System.Windows.Forms.TextBox();
+            this.txtSoN = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdTongAB = new System.Windows.Forms.RadioButton();
+            this.rdTinhTongN = new System.Windows.Forms.RadioButton();
+            this.btnKetQua = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblKetQua = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số a =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số b =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số n =";
+            // 
+            // txtSoA
+            // 
+            this.txtSoA.Location = new System.Drawing.Point(125, 31);
+            this.txtSoA.Name = "txtSoA";
+            this.txtSoA.Size = new System.Drawing.Size(181, 26);
+            this.txtSoA.TabIndex = 3;
+            this.txtSoA.Text = "0";
+            // 
+            // txtSoB
+            // 
+            this.txtSoB.Location = new System.Drawing.Point(125, 96);
+            this.txtSoB.Name = "txtSoB";
+            this.txtSoB.Size = new System.Drawing.Size(181, 26);
+            this.txtSoB.TabIndex = 4;
+            this.txtSoB.Text = "0";
+            // 
+            // txtSoN
+            // 
+            this.txtSoN.Location = new System.Drawing.Point(125, 155);
+            this.txtSoN.Name = "txtSoN";
+            this.txtSoN.Size = new System.Drawing.Size(181, 26);
+            this.txtSoN.TabIndex = 5;
+            this.txtSoN.Text = "0";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdTinhTongN);
+            this.groupBox1.Controls.Add(this.rdTongAB);
+            this.groupBox1.Location = new System.Drawing.Point(84, 224);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 130);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chọn phép tính";
+            // 
+            // rdTongAB
+            // 
+            this.rdTongAB.AutoSize = true;
+            this.rdTongAB.Location = new System.Drawing.Point(29, 47);
+            this.rdTongAB.Name = "rdTongAB";
+            this.rdTongAB.Size = new System.Drawing.Size(160, 24);
+            this.rdTongAB.TabIndex = 0;
+            this.rdTongAB.TabStop = true;
+            this.rdTongAB.Text = "Tính tổng của a, b";
+            this.rdTongAB.UseVisualStyleBackColor = true;
+            // 
+            // rdTinhTongN
+            // 
+            this.rdTinhTongN.AutoSize = true;
+            this.rdTinhTongN.Location = new System.Drawing.Point(29, 86);
+            this.rdTinhTongN.Name = "rdTinhTongN";
+            this.rdTinhTongN.Size = new System.Drawing.Size(175, 24);
+            this.rdTinhTongN.TabIndex = 1;
+            this.rdTinhTongN.TabStop = true;
+            this.rdTinhTongN.Text = "Tính tổng từ 1 đến n";
+            this.rdTinhTongN.UseVisualStyleBackColor = true;
+            // 
+            // btnKetQua
+            // 
+            this.btnKetQua.Location = new System.Drawing.Point(152, 370);
+            this.btnKetQua.Name = "btnKetQua";
+            this.btnKetQua.Size = new System.Drawing.Size(121, 35);
+            this.btnKetQua.TabIndex = 7;
+            this.btnKetQua.Text = "Xem kết quả";
+            this.btnKetQua.UseVisualStyleBackColor = true;
+            this.btnKetQua.Click += new System.EventHandler(this.btnKetQua_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(26, 424);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Kết quả là: ";
+            // 
+            // lblKetQua
+            // 
+            this.lblKetQua.AutoSize = true;
+            this.lblKetQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKetQua.Location = new System.Drawing.Point(177, 424);
+            this.lblKetQua.Name = "lblKetQua";
+            this.lblKetQua.Size = new System.Drawing.Size(17, 25);
+            this.lblKetQua.TabIndex = 9;
+            this.lblKetQua.Text = ".";
+            // 
+            // frmBai3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 580);
+            this.Controls.Add(this.lblKetQua);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnKetQua);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtSoN);
+            this.Controls.Add(this.txtSoB);
+            this.Controls.Add(this.txtSoA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmBai3";
+            this.Text = "Bài 3";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSoA;
+        private System.Windows.Forms.TextBox txtSoB;
+        private System.Windows.Forms.TextBox txtSoN;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdTinhTongN;
+        private System.Windows.Forms.RadioButton rdTongAB;
+        private System.Windows.Forms.Button btnKetQua;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblKetQua;
+    }
+}
